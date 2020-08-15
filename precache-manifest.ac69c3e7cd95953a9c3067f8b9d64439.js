d3bcc930e4ec92e563e89f08befe8679ab2818b1
llalit <erlalit.lahoria@gmail.com>
2020-08-15 17:59:25 +0530

    Updates

---
+++ precache-manifest.ac69c3e7cd95953a9c3067f8b9d64439.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef361eb97175529d30ea9ea1a98b11a9",
+    "url": "/reactlive/index.html"
+  },
+  {
+    "revision": "3d35d0bf52f57dab962b",
+    "url": "/reactlive/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "a5c1350c8d15dd8bb2f0",
+    "url": "/reactlive/static/css/main.c96cb906.chunk.css"
+  },
+  {
+    "revision": "3d35d0bf52f57dab962b",
+    "url": "/reactlive/static/js/2.94459090.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactlive/static/js/2.94459090.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5c1350c8d15dd8bb2f0",
+    "url": "/reactlive/static/js/main.ad355214.chunk.js"
+  },
+  {
+    "revision": "1a9b05908a7fa585134e",
+    "url": "/reactlive/static/js/runtime-main.02f7c19e.js"
+  },
+  {
+    "revision": "9aede5234c34a29de6dc4ebd75e386a3",
+    "url": "/reactlive/static/media/h1.9aede523.jpg"
+  },
+  {
+    "revision": "c23e96d4a43365a725a18b2060c4d8bb",
+    "url": "/reactlive/static/media/h2.c23e96d4.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactlive/static/media/logo.5d5d9eef.svg"
+  }
+]);
